3d6b77ecb99322daa025ec8bf5cafb6e0bb3dfc3
Francisco <panchifeijoo@gmail.com>
2023-07-04 16:48:31 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import("./utils/passport/local.js");
+import cors from "cors";
 import compression from "compression";
 import MongoStore from "connect-mongo";
 const advancedOptions = { useNewUrlParser: true, useUnifiedTopology: true };
@@ -28,6 +29,7 @@ app.use(compression());
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(
   session({
     store: MongoStore.create({
